475c2ac8fa8be5ebf72d7ff978c67a180cb108ba
z004bzyf <z004bzyf@EVC00108NB>
2022-05-04 23:48:49 +0200

    Add project files.

---
+++ Prevadeni_cisel/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prevadeni_cisel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string znaky = "0123456789ABCDEF";
+            int cisloVstup; 
+            int soustava;
+            string vysledek = "";
+            string obracenyVysledek = "";
+
+            Console.WriteLine("Číslo v desítkové soustavě: ");
+            while (!int.TryParse("6378"/*Console.ReadLine()*/, out cisloVstup))
+                Console.WriteLine("Neplatné číslo, zadejte prosím znovu: ");
+
+            int mezivysledek = cisloVstup;
+
+            Console.WriteLine("Číselná soustava (2-16): ");
+            while (!int.TryParse("8"/*Console.ReadLine()*/, out soustava))
+                Console.WriteLine("Neplatné číslo, zadejte prosím znovu: ");
+
+            while (mezivysledek != 0)
+            {
+                obracenyVysledek += znaky[mezivysledek % soustava];
+                mezivysledek /= soustava;
+                
+                Console.Write(mezivysledek +"\t");
+                Console.WriteLine(mezivysledek % soustava);
+            }
+
+            for (int i = 0; i < obracenyVysledek.Length; i++)
+                vysledek += obracenyVysledek[obracenyVysledek.Length - i - 1];
+
+            Console.WriteLine("Číslo ve zvolené soustavě: " + vysledek);
+            Console.ReadKey();
+        }
+    }
+}
